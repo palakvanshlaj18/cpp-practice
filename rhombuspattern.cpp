c6bbece8e7687666021ffa90a1ddb6e57d1096e2
palakvanshlaj18 <vanshlajpalak531982@gmail.com>
2024-08-15 00:09:59 +0530

    Add files via upload

---
+++ rhombuspattern.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter the number:";
+    cin >> n;
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = i; j <= n; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j <= n; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
